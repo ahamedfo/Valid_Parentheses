99c97422f8dd8b1e3dc4fb4e39bcc04690c2a4f8
ahamedfofana <ahamedfofana12@gmail.com>
2020-04-29 17:30:00 -0400

    ValidParenthesis Solved

--- ValidParenthesis.py
+++ ValidParenthesis.py
@@ -3,19 +3,21 @@ class Solution(object):
     def isValid(self, s):
         stack = deque()
         top = ''
-        top_map = {}
         braces_map = {'}': '{', ')': '(', ']': '[' }
         for values in s:
             if values in braces_map.keys():
                 if braces_map[values] == top:
-                    top = top_map[stack.pop()]
+                        stack.pop()
+                        if len(stack) > 0:
+                            top = stack.pop()
+                            stack.append(top)
                 else:
                     return False
             else:
                 stack.append(values)
-                top_map[values] = top
                 top = values
-        stack.
+        if len(stack) > 0:
+            return False
         return True
 
 
